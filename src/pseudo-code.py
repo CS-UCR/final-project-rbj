f1ef1ccaf88b84ae2b1ac7b12009f5c3622b6d40
Rithvik <vrithvik7@gmail.com>
2023-03-01 15:33:26 -0800

    changed code

--- src/pseudo-code.py
+++ src/pseudo-code.py
@@ -1,65 +1,59 @@
+import pandas as pd
 import numpy as np
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.linear_model import LogisticRegression
 
-def gaussian_noise(data, epsilon):
-    # Add Laplacian mechanism to data using Gaussian noise.
-    sensitivity = 1 / epsilon
-    shape = data.shape
-    noise = np.random.normal(scale=sensitivity, size=shape)
-    return data + noise
+# create a small example dataset
+df = pd.DataFrame({
+    "num_feat1": [1, 2, 3, 4],
+    "num_feat2": [5, 6, 7, 8],
+    "cat_feat": ["a", "b", "c", "d"]
+})
 
-def add_noise_categorical(data, sample_percentage):
-    # Add noise to categorical data by introducing a new random value based on data.
-    new_data = []
-    for feature in data.T:
-        if np.all(np.isnan(feature)):
-            new_data.append(feature)
-            continue
-        if np.all(feature == feature[0]):
-            new_data.append(feature)
-            continue
-        if np.any(np.isnan(feature)):
-            feature = np.nan_to_num(feature, nan=np.nanmean(feature))
-        values, counts = np.unique(feature, return_counts=True)
-        if len(values) >= 50:
-            new_data.append(feature)
-            continue
-        for i in range(len(feature)):
-            if np.random.rand() < sample_percentage:
-                value_counts = list(zip(values, counts))
-                value_counts.sort(key=lambda x: x[1])
-                value_counts = value_counts[-5:]
-                value = np.random.choice([vc[0] for vc in value_counts])
-                feature[i] = value
-        new_data.append(feature)
-    return np.array(new_data).T
+# factorize categorical feature
+df["cat_feat"] = pd.factorize(df["cat_feat"])[0]
 
-def add_noise_numerical(data, sample_percentage, model='logistic_regression'):
-    # Add noise to numerical data using Laplacian mechanism and Gaussian noise.    
-    new_data = []
-    for feature in data.T:
-        if np.all(np.isnan(feature)):
-            new_data.append(feature)
-            continue
-        if np.all(feature == feature[0]):
-            new_data.append(feature)
-            continue
-        if np.any(np.isnan(feature)):
-            feature = np.nan_to_num(feature, nan=np.nanmean(feature))
-        if np.random.rand() < sample_percentage:
-            if np.random.rand() < 0.5:
-                model = LogisticRegression()
-            else:
-                model = RandomForestClassifier()
-            X = np.delete(data, np.argwhere(data == feature).T[0], axis=1)
-            y = feature.copy()
-            y[np.isnan(y)] = 0
-            y[np.logical_not(np.isnan(y))] = 1
-            model.fit(X[~np.isnan(y)], y[~np.isnan(y)])
-            predictions = model.predict_proba(X[np.isnan(y)])
-            noise = gaussian_noise(predictions[:, 1], 1)
-            noise = np.clip(noise, -0.5, 0.5)
-            feature[np.isnan(feature)] = model.predict_proba(X[np.isnan(y)])[:, 1] + noise
-        new_data.append(feature)
-    return np.array(new_data).T
+# add noise to categorical feature
+def add_noise_categorical(feature, sample_pct, noise_scale):
+    """
+    Add Gaussian noise to a categorical feature.
+    """
+    n_samples = int(len(feature) * sample_pct / 100)
+    samples = np.random.choice(feature, size=n_samples, replace=True)
+    noise = np.random.normal(scale=noise_scale, size=len(samples))
+    new_samples = np.clip(samples + noise, np.min(feature), np.max(feature))
+    new_feature = feature.copy()
+    new_feature[np.isin(feature, samples)] = new_samples
+    return new_feature
+
+X_categorical = df["cat_feat"].values
+X_noisy_categorical = add_noise_categorical(X_categorical, 50, 0.1)
+
+# add noise to numerical features
+def add_noise_numerical(X, sample_pct, noise_scale):
+    """
+    Add Gaussian noise to numerical features in a dataset.
+    """
+    X_noisy = X.copy()
+    numerical_cols = [0, 1]  # column indices for numerical columns
+    for col in numerical_cols:
+        feature = X[:, col]
+        n_samples = int(len(feature) * sample_pct / 100)
+        samples = np.random.choice(feature, size=n_samples, replace=True)
+        noise = np.random.normal(scale=noise_scale, size=len(samples))
+        new_samples = np.clip(samples + noise, np.min(feature), np.max(feature))
+        X_noisy[:, col][np.isin(feature, samples)] = new_samples
+    return X_noisy
+
+numerical_cols = ["num_feat1", "num_feat2"]
+X_numerical = df[numerical_cols].values
+X_noisy_numerical = add_noise_numerical(X_numerical, 50, 0.1)
+
+# combine noisy features with original features
+df_noisy = pd.DataFrame(X_noisy_categorical, columns=["cat_feat"])
+df_noisy[numerical_cols] = X_noisy_numerical
+
+print(df.head())
+print(df_noisy.head())
+
+# check if the shapes are the same
+print(df.shape)
+print(df_noisy.shape)
